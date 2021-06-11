58bbc5788e2d9a53b348f34ea516b8f3853ab517
ManonmaniT <37766083+ManonmaniT@users.noreply.github.com>
2021-06-11 19:54:55 +0530

    updated code with ordersummary verifcation

--- src/test/java/com/qmetry/testng_demo/NewTest.java
+++ src/test/java/com/qmetry/testng_demo/NewTest.java
@@ -22,6 +22,8 @@ public class NewTest {
 
 	String subprodcutbeingselected = "";
 
+	String totalvalueduringcheckout = "";
+
 	@BeforeClass
 	public void beforeClass() {
 
@@ -58,6 +60,9 @@ public class NewTest {
 		Reporter.log("Main products category availabel are ..." + listofmainproductslabel.toString());
 	}
 
+	
+	
+	//Function to get random int for random selection from the list of products for operation 
 	public String getrandonintforselection(ArrayList<String> list) {
 		int max = 0;
 		int min = 0;
@@ -77,91 +82,110 @@ public class NewTest {
 	public void orderandcheckout_happypath_e2e() {
 
 		Reporter.log("----Test case execution for happy path - e2e testing------");
-
-		Reporter.log("Step 1----Getting the list of main product cateogory available");
-
-		String productname = getrandonintforselection(listofmainproductslabel);
-
-		String xpath = "///*[contains(@id, '" + productname + "')]";
-
-		Reporter.log("Step 2----Selection of main product category to proceed with");
-
-		driver.findElement(By.xpath(xpath)).click();
-
-		Reporter.log("Step 3----Selection of sub product category to proceed with");
-
-		// Though of random selection here as well but due to time constraint proceeding
-		// with list item if available
-
-		java.util.List<WebElement> subproducts = driver.findElements(By.xpath("//*[contains(@class, 'imgProduct')]"));
-
-		if (subproducts.size() > 0) {
-
-			subprodcutbeingselected = driver.findElement(By.xpath("(//*[contains(@class, 'imgProduct')])[1]"))
-					.getText();
-			driver.findElement(By.xpath("(//*[contains(@class, 'imgProduct')])[1]")).click();
-
-		}
 		
+			// As the wwebpage is dynamic is it better to get dynamic lsitof products and do
+			// operation rather than hard coding
 		
-
-		Reporter.log("Step 3----Selection of sub product category and proceeding for checkout");
-		
-		//Click add to cart
-		driver.findElement(By.xpath("//button[contains(@name, 'save_to_cart')]")).click();
-		
-		//Click checkout
-		
-		driver.findElement(By.xpath("(//*[contains(@id, 'checkOutPopUp')])[2]")).click();
-
-
-		Reporter.log("Step 4----Checkout as user already exists");
-		
-		
-        String pageheader = driver.findElement(By.xpath("/html/body/div[3]/section/article/h3")).getText();
-        
-         
-        
-        assertTrue(pageheader.contains("ORDER PAYMENT"),"Application landed successfully ");
-		
-        		
-        driver.findElement(By.xpath("//input[contains(@name, 'usernameInOrderPayment')]")).sendKeys("testuser_123");
-
-		driver.findElement(By.xpath("//input[contains(@name, 'usernameInOrderPayment')]")).sendKeys("Test@123");
-
-		
-
-		driver.findElement(By.xpath("//*[contains(@id, 'next_btn')]")).click();
-		
-		
-
-		// verif for payment method display
-
-		String headertext = driver.findElement(By.xpath("//*[@id=\"detailslink\"]/label[2]")).getText();
-
-		driver.findElement(By.xpath("//input[contains(@name, 'safepay_username')]")).sendKeys("testuser_123");
-
-		driver.findElement(By.xpath("//input[contains(@name, 'safepay_password')]")).sendKeys("Test@123");
-
-		driver.findElement(By.xpath("//*[contains(@id, 'pay_now_btn_SAFEPAY')]")).click();
-
-		headertext = driver.findElement(By.xpath("//*[@id=\"orderPaymentSuccess\"]/h2/span")).getText();
-
-		// Thank you for buying with Advantage
-
-		String ath = "//*[@id=\"orderPaymentSuccess\"]/p";
-
-		// Your tracking number is
-
-		// username //*[@id="orderPaymentSuccess"]/div/div[1]/div/div[1]/label
-
-		// tiatol =//*[@id="orderPaymentSuccess"]/div/div[3]/div[3]/label/a
+			Reporter.log("Step 1----Getting the dynamic list of main product cateogory available");
+	
+						String productname = getrandonintforselection(listofmainproductslabel);
+				
+						String xpath = "///*[contains(@id, '" + productname + "')]";
+	
+			Reporter.log("Step 2----Selection of main product category to proceed with");
+	
+						driver.findElement(By.xpath(xpath)).click();
+				
+						Reporter.log("Step 3----Selection of sub product category to proceed with");
+				
+						// Though of random selection here as well but due to time constraint proceeding
+						// with list item if available
+				
+						java.util.List<WebElement> subproducts = driver.findElements(By.xpath("//*[contains(@class, 'imgProduct')]"));
+				
+						if (subproducts.size() > 0) {
+				
+							subprodcutbeingselected = driver.findElement(By.xpath("(//*[contains(@class, 'imgProduct')])[1]"))
+									.getText();
+							driver.findElement(By.xpath("(//*[contains(@class, 'imgProduct')])[1]")).click();
+				
+						}
+	
+			Reporter.log("Step 3----Selection of sub product category and proceeding for checkout");
+	
+						// Click add to cart
+						driver.findElement(By.xpath("//button[contains(@name, 'save_to_cart')]")).click();
+				
+						// Click checkout
+				
+						driver.findElement(By.xpath("(//*[contains(@id, 'checkOutPopUp')])[2]")).click();
+				
+						totalvalueduringcheckout = driver.findElement(By.xpath("//*[@id=\"userCart\"]/div[2]/label[2]/span")).getText();
+				
+						Reporter.log("Step 4----Checkout as user already exists");
+				
+						String pageheader = driver.findElement(By.xpath("/html/body/div[3]/section/article/h3")).getText();
+				
+						assertTrue(pageheader.contains("ORDER PAYMENT"), "Application landed successfully ");
+				
+						driver.findElement(By.xpath("//input[contains(@name, 'usernameInOrderPayment')]")).sendKeys("testuser_123");
+				
+						driver.findElement(By.xpath("//input[contains(@name, 'usernameInOrderPayment')]")).sendKeys("Test@123");
+				
+						driver.findElement(By.xpath("//*[contains(@id, 'next_btn')]")).click();
+	
+			Reporter.log("Step 4----Checkout as user already exists");
+	
+						pageheader = driver.findElement(By.xpath("/html/body/div[3]/section/article/h3")).getText();
+				
+						assertTrue(pageheader.contains("ORDER METHOD"), "User is in ORDER PAYMENT ");
+				
+						// verif for payment method display
+				
+						String headertext = driver.findElement(By.xpath("//*[@id='detailslink']/label[2]")).getText();
+				
+						assertTrue(pageheader.contains("PAYMENT METHOD"), "User is in  PAYMENT METHOD");
+				
+			Reporter.log("Step 5---choose payment and order");
+	
+						driver.findElement(By.xpath("//input[contains(@name, 'safepay_username')]")).sendKeys("testuser_123");
+				
+						driver.findElement(By.xpath("//input[contains(@name, 'safepay_password')]")).sendKeys("Test@123");
+				
+						driver.findElement(By.xpath("//*[contains(@id, 'pay_now_btn_SAFEPAY')]")).click();
+	
+			Reporter.log("Step 5---Verification of order summary page");
+	
+						headertext = driver.findElement(By.xpath("//*[@id='orderPaymentSuccess']/h2/span")).getText();
+				
+						assertTrue(pageheader.contains("Thank you for buying with Advantage"),
+								"ORDER SUMARY contains -->Thank you message");
+				
+						String ordertrackigntext = driver.findElement(By.xpath("//*[@id='orderPaymentSuccess']/p")).getText();
+				
+						assertTrue(ordertrackigntext.contains("Your tracking number is"),
+								"ORDER SUMARY contains -->ORDER TRACKING INFO");
+				
+						String usernameinfo = driver.findElement(By.xpath("//*[@id='orderPaymentSuccess']/div/div[1]/div/div[1]/label"))
+								.getText();
+				
+						assertTrue(usernameinfo.contains("testuser"), "ORDER SUMARY contains -->USERINFO");
+				
+						String userino = driver.findElement(By.xpath("//*[@id='orderPaymentSuccess']/p")).getText();
+				
+						assertTrue(ordertrackigntext.contains("Your tracking number is"),
+								"ORDER SUMARY contains -->ORDER TRACKING INFO");
+				
+						String totalvalueduring_ordersummary = driver
+								.findElement(By.xpath("//*[@id='orderPaymentSuccess']/div/div[3]/div[3]/label/a")).getText();
+				
+						assertTrue(totalvalueduringcheckout.contains(totalvalueduring_ordersummary),
+								"ORDER SUMARY toatl value matches with during checkout");
+		 
 	}
 
 	// Test case to veirfy applciaiton lainch
-
-	// Test case to place order and checkout = Happy Path
-
+ 
 	// Test case to register user
 
 	// Test case to modify order
